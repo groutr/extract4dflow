5dc7f6552ab8da74e5866972a5c09edc48266dde
Ryan Grout <ryan.grout@noaa.gov>
2021-06-30 15:39:32 +0000

    Print progress updates to stdout

--- scripts/tidalfreq.py
+++ scripts/tidalfreq.py
@@ -16,26 +16,35 @@ def read_freq_map(path):
 
 
 def main(args):
+    print("Reading input files...", end='')
     src = Fort53Parser(args.fort15, args.fort53)
     src._parse_freqs()
     src._parse_grd()
     pli = read_pli(args.pli)
+    print("done")
 
     units = [('astronomic component', '-'),
              ('waterlevelbnd amplitude', 'm'),
              ('waterlevelbnd phase', 'deg')]
 
     with BCFileWriter(args.output) as bc_out:
+        print("Finding nearest stations...", end='')
         _, idx = kd_nearest_neighbor(src.node_coords(), pli['values'])
         idx_s = idx.argsort()
-        nodes = src.nodes[idx[idx_s]]['jn']
+        nodes = src.nodes[idx[idx_s]]['jn'].astype(int)
+        print("done")
         if args.frequencies is None:
             freqs = {k: k for k in src.freq}
         else:
             freqs = read_freq_map(args.frequencies)
+        print("Reading frequencies...", len(nodes), len(freqs))
+        pli_names = pli['index']
         for i, (n, F) in enumerate(src.read_freqs(nodes=nodes, freqs=list(freqs.keys()))):
             data = map(cons, freqs.values(), F)
-            bc_out.add_forcing(pli['index'][idx_s[i]], "astronomic", units, data)
+            name = pli_names[idx_s[i]]
+            bc_out.add_forcing(name, "astronomic", units, data)
+            print("Station", name, end='\r')
+        print()
 
 
 def get_options():
